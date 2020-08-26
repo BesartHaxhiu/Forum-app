3131a3653802a78444787eda1430ea3f32cc703b
Besart Haxhiu <bh40793@ubt-uni.net>
2020-08-26 15:23:33 +0200

    Models Factories Modified

--- database/factories/QuestionFactory.php
+++ database/factories/QuestionFactory.php
@@ -6,7 +6,20 @@ use App\Model\Question;
 use Faker\Generator as Faker;
 
 $factory->define(Question::class, function (Faker $faker) {
+    $title = $faker->sentence;
     return [
-        //
+        'title' => $title,
+        'slug' => str_slug($title), // Slug is the same with title except it removes spaces
+                                    //That means we can use title on slug too, to do that we declare
+                                    //title at the top before return[] and then use it here
+                                    // str_slug then takes the title string and it transforms it into 
+                                    //slug requirements. P.S forum app -> with str_slug -> forum-app
+        'body' => $faker->text,
+        'category_id' => function(){
+            return Category::all()->random();
+        },
+        'user_id' => function(){
+            return User::all()->random();
+        }
     ];
 });
--- database/factories/ReplyFactory.php
+++ database/factories/ReplyFactory.php
@@ -7,6 +7,12 @@ use Faker\Generator as Faker;
 
 $factory->define(Reply::class, function (Faker $faker) {
     return [
-        //
+        'body' => $faker->text,
+        'question_id' => function(){
+            return Question::all()->random();
+        },
+        'user_id' => function(){
+            return User::all()->random(); // nese sban provo \App\User...
+        }
     ];
 });
