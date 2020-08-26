04f41e957c415f1aa4077b382abfdda963241e39
Besart Haxhiu <bh40793@ubt-uni.net>
2020-08-26 12:15:49 +0200

    Models Relationship between Question and Reply

--- app/Model/Question.php
+++ app/Model/Question.php
@@ -6,5 +6,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class Question extends Model
 {
-    //
+    //Creating the relationship between the models created.
+
+    public function user() // A question belongs to any user or any user can create a question
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function replies() // a question has many replies
+    {
+        return $this->hasMany(Reply::class);
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
--- app/User.php
+++ app/User.php
@@ -36,4 +36,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function question() // The relationship created for user model.
+    {
+        return $this->hasMany(Question::class);
+    }
 }
